5e1965ceb662a4752d0e44c92ae357d46dbb5dfd
Emmanuel <osaroimarhiagbe@gmail.com>
2024-02-04 17:56:14 -0600

    adding background image

--- app/assets/screens/WeatherScreen.js
+++ app/assets/screens/WeatherScreen.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import Screen from "../../../components/Screen/Screen";
-import { Text, View } from 'react-native'
+
+import { SafeAreaView, Text, View, StyleSheet, Platform, StatusBar, ImageBackground } from 'react-native'
 
 const WeatherScreen = () => {
   return (
-    <Screen>
 
-</Screen>
+     
+           <ImageBackground style={styles.screen} source={require('../../assets/background.jpg')}>
+           </ImageBackground>
+     
+ 
   )
 }
+
+
+
+const styles = StyleSheet.create({
+    screen:{
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        flex:1
+    },
+})
 
 export default WeatherScreen
